bde9b91133e8181150106ec9a5dca5fd1c222743
P Sai Vamsi <saivamsi@devopsenabler.com>
2021-08-05 02:05:16 +0000

    Added create_user_admin.py

---
+++ create_user_admin.py
@@ -0,0 +1,16 @@
+from DOEAssessmentApp.DOE_models.company_user_details_model import Companyuserdetails
+from werkzeug.security import generate_password_hash
+from DOEAssessmentApp import db
+
+try:
+    checkifuseradminexists = Companyuserdetails.query.filter(Companyuserdetails.empname == "admin").one_or_none()
+    if checkifuseradminexists is None:
+        add_user_admin = Companyuserdetails("1", "admin", "admin", "admin@testmail.com", generate_password_hash("admin"), 0,
+                                            None)
+        db.session.add(add_user_admin)
+        db.session.commit()
+        print("User admin added with password admin successfully !!", flush=True)
+    else:
+        print("User admin already exists !!", flush=True)
+except Exception as e:
+    print(e)
