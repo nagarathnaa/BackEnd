79b9bae3151d7f4ff0db4e6be31bca0cc9a32b89
Srijib Bhattacharyya <srijib-bhattacharyya@devopsenabler.com>
2021-08-11 13:17:02 +0530

    trying to resolve bugs found in fresh installation of the app

--- DOEAssessmentApp/DOE_views/company_details_view.py
+++ DOEAssessmentApp/DOE_views/company_details_view.py
@@ -2,8 +2,6 @@ import uuid
 from flask import *
 from DOEAssessmentApp import db
 from DOEAssessmentApp.DOE_models.company_details_model import Companydetails
-from DOEAssessmentApp.DOE_models.company_user_details_model import Companyuserdetails
-from werkzeug.security import generate_password_hash
 
 companydetails = Blueprint('companydetails', __name__)
 
@@ -55,33 +53,27 @@ def companydetail():
     """
     try:
         if request.method == "GET":
-            data = Companydetails.query.all()
-            result = [{col: getattr(d, col) for col in colscompanydetails} for d in data]
-            return make_response(jsonify({"data": result[0]})), 200
+            data = Companydetails.query.one_or_none()
+            if data is not None:
+                result = [{col: getattr(d, col) for col in colscompanydetails} for d in data]
+                return make_response(jsonify({"data": result[0]})), 200
         elif request.method == "POST":
             res = request.get_json(force=True)
             cname = res['CompanyName']
             regadrs = res['RegisteredAddress']
             billadrs = res['BillingAddress']
             regkey = str(uuid.uuid4())
-            eid = res['EmployeeId']
-            ename = res['EmployeeName']
-            erole = res['EmployeeRole']
-            email = res['EmployeeEmail']
             existing_company = Companydetails.query.filter(Companydetails.companyname == cname).one_or_none()
             if existing_company is None:
                 compdet = Companydetails(cname, regadrs, billadrs, regkey)
                 db.session.add(compdet)
                 db.session.commit()
-                compuserdet = Companyuserdetails(eid, ename, erole, email,
-                                                 generate_password_hash(res['EmployeePassword']), compdet.id, None)
-                db.session.add(compuserdet)
-                db.session.commit()
+                # send a mail to company admin with registration key which will be validated first time after install
                 return make_response(jsonify({"msg": f"Company details with Company Name {cname} "
-                                                         f"successfully inserted."})), 201
+                                                     f"successfully inserted."})), 201
             else:
                 return make_response(jsonify({"msg": f"Company details with Company Name {cname} "
-                                                         f"already exists."})), 400
+                                                     f"already exists."})), 400
     except Exception as e:
         return make_response(jsonify({"msg": str(e)})), 500
 
@@ -93,7 +85,7 @@ def updatecompanydetails():
         cmpny_id = res['id']
         data = Companydetails.query.filter_by(id=cmpny_id)
         if data.first() is None:
-            return make_response(jsonify({"msg": "Incorrect ID"})), 404
+            return make_response(jsonify({"msg": "No company details found !!"})), 404
         else:
             if request.method == 'POST':
                 result = [{col: getattr(d, col) for col in colscompanydetails} for d in data]
